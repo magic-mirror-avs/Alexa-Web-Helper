dcb35f09ddce1823aa973bc5d6f822b57d4d3916
Dolan Miu <dolan_miu@hotmail.com>
2017-04-23 03:36:40 +0100

    made it deploy and fixed prod build issues

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AmazonService } from '../amazon.service';
 
 @Component({
     selector: 'app-start',
     templateUrl: './start.component.html',
     styleUrls: ['./start.component.scss']
 })
-export class StartComponent implements OnInit {
+export class StartComponent {
 
-    constructor() { }
-
-    ngOnInit() {
+    constructor(private amazonService: AmazonService) {
     }
 
+    public login(): void {
+        this.amazonService.login();
+    }
 }
